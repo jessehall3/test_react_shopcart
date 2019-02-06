4fe3a37600939935690501782efeb30a75c69dba
Jesse Hall <jessehall3@gmail.com>
2019-02-06 14:12:55 -0500

    Showing fields for add product view. Fields have associated states.

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddProduct() {
+  const [name, setName] = useState()
+  const [price, setPrice] = useState()
+  const [description, setDescription] = useState()
+  const [image, setImage] = useState()
+
+  const handleChangeName = e => {
+    setName(e.target.value)
+  }
+
+  const handleChangePrice = e => {
+    setPrice(e.target.value)
+  }
+
+  const handleChangeDescription = e => {
+    setDescription(e.target.value)
+  }
+
+  const handleChangeImage = e => {
+    setImage(e.target.value)
+  }
+
   return (
-    <h1>Add Product</h1>
+    <div>
+      <form>
+        <h1>Add Product</h1>
+        <div>
+          <label>Name:</label>
+          <input required onChange={handleChangeName} />
+        </div>
+        <div>
+          <label>Price in $:</label>
+          <input required onChange={handleChangePrice} />
+        </div>
+        <div>
+          <label>Description:</label>{' '}
+          <textarea required onChange={handleChangeDescription} />
+        </div>
+        <div>
+          <label>Image URL:</label>
+          <input required onChange={handleChangeImage} />
+        </div>
+        <input type="submit" value="Add" className="button" />
+      </form>
+    </div>
   )
 }
 
