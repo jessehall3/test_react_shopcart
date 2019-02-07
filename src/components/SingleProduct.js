719a9a00497294822eac37c06166a3ccb1b4dda6
Jesse Hall <jessehall3@gmail.com>
2019-02-07 10:24:19 -0500

    WIP Now showing a quantity dropdown to be used with the add to cart button.

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -5,14 +5,32 @@ function SingleProduct(props) {
     props.addToCart({ product: props.product, quantity: 1 })
   }
 
+  const quantitySelect = (len=20) => {
+    const numArray =  Array.from({length: len}, (x,i) => i + 1)
+    const options = numArray.map( num => {
+      return  <option value={num}>{num}</option>
+    })
+    return(
+      <div>
+        <label htmlFor="quantity">Quantity</label>
+        <select name="quantity">
+        {options}
+        </select>
+      </div>
+    )
+  }
+
   const productDiv = (product) => {
-    return <div className="single-product">
-    <img src={product.image} />
-    <h2>{product.name}</h2>
-    <button onClick={addToCart}>Add to cart</button>
-    <p className="description">{product.description}</p>
-    <p className="price">${product.price}</p>
-    </div>
+    return(
+      <div className="single-product">
+      <img src={product.image} />
+      <h2>{product.name}</h2>
+      <p className="description">{product.description}</p>
+      <p className="price">${product.price}</p>
+      {quantitySelect()}
+      <button onClick={addToCart}>Add to cart</button>
+      </div>
+    )
   }
 
   return props.product ? productDiv(props.product) : <p>No Product Found</p>
